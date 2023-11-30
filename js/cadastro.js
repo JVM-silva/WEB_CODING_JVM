5944bfcc37f442c18705a6339d93e85662e13058
Márcio Sobel <marcinhosobel@hotmail.com>
2023-11-29 22:49:03 -0300

    Arrumar tela de cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,56 +1,59 @@
 $(document).ready(function () {
-    const email = $('#email'),
-          cpf = $('#cpf'),
-          senha = $('#password'),
-          confSenha = $('#confirm-password'),
-          btnSubmit = $('#botao-login'),
-          alreadyExists = $('#already-exists');
-
-
+    
     const hasLetter = (txt) => {
         return /[a-ç0-9]/i.test(txt);
     }
 
-    btnSubmit.on('click', (e) => {
-        e.preventDefault();
+    const showError = (str) => {
+        $('#error').text(str);
+        $('#error').css('display', 'block');
+    }
+    
 
-        alreadyExists.css('display', 'none');
-        let isInvalid = false;
-        // checa se algum campo está vazio
-        $('#info-cadastro').children('input').each((i, element) => {
-            const txt = $(element).val();
+    $('#botao-login').on('click', () => {
+        $('#form-cadastro input').trigger('click');
 
-            if (!hasLetter(txt)) {
-                $(element).css('border', '3px solid var(--vermelho)');
-                isInvalid = true;
+        let invalid = false;
+
+        $('#form-cadastro input').each((i, e) => {
+            $e = $(e);
+            if (!hasLetter($e.val())) {
+                $e.css('border', '3px solid var(--vermelho)');
+                invalid = true;
             }
         });
-        if (isInvalid) return;
-
-        // checa se a senha está correta
-        if (senha.val() != confSenha.val()) {
-            confSenha.css('border', '3px solid var(--vermelho)');
+        if (invalid) {
+            showError('Por favor, preencha os campos obrigatórios');
             return;
         }
 
-        // checa se já há um cadastro com o mesmo e-mail
-        const cadastros = localStorage.getItem('email');
-        let lstEmails = [],
-            lstSenhas = [];
-
-        if (cadastros != null) {
-            lstEmails = JSON.parse(cadastros);
-            
-            for (let v of arr) {
-                if (email.val() == v) {
-                    alreadyExists.css('display', 'inline-block');
-                    return;
-                }
-            }
+        if ($('#password').val() != $('#confirm-password').val()) {
+            showError('Verifique se a senha está correta');
+            $('#confirm-password').css('border', '3px solid var(--vermelho)');
+            return;
         }
-        lstEmails.push(email.val());
-        lstSenhas.push(senha.val());
-        localStorage.setItem('email', JSON.stringify(lstEmails));
-        localStorage.setItem('senha', JSON.stringify(lstSenhas));
+
+        let dados = [];
+        if (localStorage.getItem('credenciais') != null) {
+            dados = JSON.parse(localStorage.getItem('credenciais'));
+        }
+
+        const dadosObj = {
+            'email': $('#email').val(),
+            'cpf': $('#cpf').val(),
+            'senha': $('#password').val()
+        };
+        dados.push(dadosObj);
+        localStorage.setItem('credenciais', JSON.stringify(dados));
+        location.href = 'login.html';
+    });
+
+    $('#form-cadastro input').each((i, e) => {
+        $(e).on('click', () => {
+            if ($(e).css('border') === '3px solid var(--vermelho)') {
+                $('#error').css('display', 'none');
+                $(e).css('border', '3px solid var(--verde)');
+            }
+        });
     });
 });
