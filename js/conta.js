0fa0dfcc2953a41721cd1d5084f44ce73ad6cabc
Márcio Sobel <marcinhosobel@hotmail.com>
2023-11-30 17:12:18 -0300

    Adicionar configurações de conta

--- js/conta.js
+++ js/conta.js
@@ -1,4 +1,31 @@
 $(document).ready(function () {
+    //localStorage.clear();
+
+    const accountData = JSON.parse(sessionStorage.getItem('login'));
+
+    if (accountData.tipo != 'admin') { $('.admin').css('display', 'none'); }
+
+    $('#email').text(accountData.email);
+    $('#cpf').text(accountData.cpf);
+
+
+    $('#apagar-dados').on('click', () => {
+        sessionStorage.clear();
+        localStorage.clear();
+        location.href = 'login.html';
+    });
+
+    $('#apagar-conta').on('click', () => {
+        const allData = JSON.parse(localStorage.getItem('credenciais')),
+              currAcc = accountData,
+              dataPos = allData.map(e => e.email).indexOf(currAcc.email);
+        
+        let newData = allData;
+        if (dataPos > -1) { newData.splice(dataPos, 1); }
+        localStorage.setItem('credenciais', JSON.stringify(newData));
+        location.href = 'index.html';
+    });
+
     $('#deslogar').on('click', () => {
         localStorage.removeItem('loginSalvo');
         sessionStorage.removeItem('login');
